7d3a8c562e6ba7cc5637dfe882becfdc1616ddc9
marco66colombo <80001772+marco66colombo@users.noreply.github.com>
2021-04-06 10:53:58 +0200

    New classes: MarketBoard (with tests), Marble (only the definition of the class) and the different colors of marbles (only the definition of the class and the method toString) New exceptions: IllegalMarketException

---
+++ AM19/AM19/src/main/java/it/polimi/ingsw/MarbleRed.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw;
+
+/**
+ * This class implements Marble.
+ * It represents the red marble.
+ */
+public class MarbleRed implements Marble {
+
+    @Override
+    public String toString() {
+        return "MarbleRed";
+    }
+}
---
+++ AM19/AM19/src/main/java/it/polimi/ingsw/MarketBoard.java
@@ -0,0 +1,177 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
+/**
+ * This class represents the market board which contains the tray with the marbles and the slide.
+ */
+public class MarketBoard {
+
+    /**
+     * nRows represents the number of rows of the tray.
+     */
+    private int nRows;
+    /**
+     * nColumns represents the number of columns of the tray.
+     */
+    private int nColumns;
+    /**
+     * tray represents the market tray.
+     */
+    private ArrayList<Marble> tray;
+    /**
+     * slide represents the marble which is temporarily out of the tray.
+     */
+    private Marble slide;
+
+    /**
+     * This the constructor of this class, no parameters are needed.
+     * It crates the tray and the slide and it randomizes the position of the marbles
+     */
+    public MarketBoard() {
+        nRows = 3;
+        nColumns = 4;
+        tray = new ArrayList<>((nRows*nColumns)+1);
+        int i;
+        for(i=0; i<2; i++){
+            tray.add(new MarbleBlue());
+        }
+        for(i=0; i<2; i++){
+            tray.add(new MarbleGray());
+        }
+        for(i=0; i<2; i++){
+            tray.add(new MarblePurple());
+        }
+        for(i=0; i<2; i++){
+            tray.add(new MarbleYellow());
+        }
+        for(i=0; i<1; i++){
+            tray.add(new MarbleRed());
+        }
+        for(i=0; i<4; i++){
+            tray.add(new MarbleWhite());
+        }
+        Collections.shuffle(tray);
+        slide = tray.remove(nRows*nColumns);
+    }
+
+    /**
+     * This method gets the marble which is temporarily out of the tray.
+     * @return Marble
+     */
+    public Marble getSlide(){
+        return slide;
+    }
+
+    /**
+     * This method gets a list which contains the row (with index 'row') of the tray.
+     * An IllegalMarketException is thrown if the row with index 'row' does not exist.
+     * @param row the index of the row
+     * @return an ArrayList which contains the row
+     * @throws IllegalMarketException
+     */
+    public ArrayList<Marble> getRow(int row) throws IllegalMarketException{
+
+        if(row<0 || row>=nRows)
+            throw new IllegalMarketException("This row does not exist!");
+
+        ArrayList<Marble> list = new ArrayList<>(nColumns);
+        int i;
+        int j = row*nColumns;
+        int k = j + nColumns - 1;
+        for(i=j; i<=k; i++){
+            list.add(tray.get(i));
+        }
+        return list;
+    }
+
+    /**
+     * This method returns a list which contains the row (with index 'row') of the tray and
+     * it modifies the tray and the slide according to the rules of the game.
+     * An IllegalMarketException is thrown if the row with index 'row' does not exist.
+     * @param row the index of the row
+     * @return an ArrayList which contains the row
+     * @throws IllegalMarketException
+     */
+    public ArrayList<Marble> takeRow(int row) throws IllegalMarketException{
+
+        if(row<0 || row>=nRows)
+            throw new IllegalMarketException("This row does not exist!");
+
+        ArrayList<Marble> list = new ArrayList<>(nColumns);
+        Marble temp;
+        int i;
+        int j = row*nColumns;
+        int k = j + nColumns - 1;
+        temp = tray.get(j);
+        for(i=j; i<=k; i++){
+            list.add(tray.get(i));
+            if(i != k)
+                tray.set(i, tray.get(i+1));
+        }
+        tray.set(k, slide);
+        slide = temp;
+        return list;
+    }
+
+    /**
+     *This method gets a list which contains the column (with index 'column') of the tray.
+     *An IllegalMarketException is thrown if the column with index 'column' does not exist.
+     * @param column the index of the column
+     * @return an ArrayList which contains the column
+     * @throws IllegalMarketException
+     */
+    public ArrayList<Marble> getColumn(int column) throws IllegalMarketException{
+
+        if(column<0 || column>=nColumns)
+            throw new IllegalMarketException("This column does not exist!");
+
+        ArrayList<Marble> list = new ArrayList<>(nRows);
+        int i;
+        for(i=0; i<nRows; i++){
+            list.add(tray.get(column + i*nColumns));
+        }
+        return list;
+    }
+
+    /**
+     * This method returns a list which contains the column (with index 'column') of the tray and
+     * it modifies the tray and the slide according to the rules of the game.
+     * An IllegalMarketException is thrown if the column with index 'column' does not exist.
+     * @param column the index of the column
+     * @return an ArrayList which contains the column
+     * @throws IllegalMarketException
+     */
+    public ArrayList<Marble> takeColumn(int column) throws IllegalMarketException{
+
+        if(column<0 || column>=nColumns)
+            throw new IllegalMarketException("This column does not exist!");
+
+        ArrayList<Marble> list = new ArrayList<>(nRows);
+        Marble temp;
+        int i;
+        int j = nRows-1;
+        temp = tray.get(column);
+        for(i=0; i<=j; i++){
+            list.add(tray.get(column + i*nColumns));
+            if(i != j)
+                tray.set(column + i*nColumns, tray.get(column + (i+1)*nColumns));
+        }
+        tray.set(column + j*nColumns, slide);
+        slide = temp;
+        return list;
+    }
+
+    /**
+     * This method gets the tray.
+     * @return an ArrayList which contains the tray
+     */
+    public ArrayList<Marble> getTray(){
+        ArrayList<Marble> list = new ArrayList<>(tray.stream().collect(Collectors.toList()));
+        return list;
+    }
+
+
+}
