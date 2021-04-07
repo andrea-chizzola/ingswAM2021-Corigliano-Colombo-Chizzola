8f5f96ffc3ea7485eba938a46a9043309774fcd5
andrea-chizzola <andrea.chizzola@mail.polimi.it>
2021-04-07 15:12:34 +0200

    Added  empty GameBoard class

---
+++ AM19/AM19/src/main/java/it/polimi/ingsw/GameBoard.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw;
+
+import java.util.ArrayList;
+
+/**
+ * public class representing the game board containing the personal boards related to each player
+ */
+public class GameBoard {
+
+
+
+}
