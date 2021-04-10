79d1541bccdaf4e91e29daab84865b698443c832
Davide <davide.corigliano@mail.polimi.it>
2021-04-10 17:34:11 +0200

    Added Modifications of player's boards, and SpecialEffects of LeaderCards.

---
+++ AM19/AM19/src/test/java/it/polimi/ingsw/CardTest.java
@@ -0,0 +1,55 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.xmlParser.CardParser;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * this class is used to test the implemented method of the abstract class Card
+ */
+public class CardTest {
+    String DevTest = "src\\test\\java\\it\\polimi\\ingsw\\DevCardsTest.xml";
+    String LeaderTest = "src\\test\\java\\it\\polimi\\ingsw\\LeaderCardsTest.xml";
+
+    CardParser parser;
+
+    @BeforeEach
+    public void setUp(){
+        parser = CardParser.instance();
+    }
+
+    @Test
+    public void getProductionDevTest(){
+        //creation of a card
+        List<DevelopmentCard> cards = parser.parseDevelopmentCard(DevTest);
+        DevelopmentCard card = cards.get(0);
+
+        //construction of card production
+        LinkedList<ResQuantity> materials = new LinkedList<>();
+        materials.add(new ResQuantity(new Coin(), 1));
+        LinkedList<ResQuantity> products = new LinkedList<>();
+        products.add(new ResQuantity(new Faith(), 1));
+
+        Production effect = new Production(materials, products);
+
+        assertEquals(card.getSpecialEffect().getProduction(), effect);
+
+    }
+    @Test
+    public void getProductionLeaderTest(){
+        //creation of a card
+        List<LeaderCard> cards = parser.parseLeaderCard(LeaderTest);
+        LeaderCard card = cards.get(0);
+
+        assertEquals(card.getSpecialEffect().getProduction(), new Production(
+                new LinkedList<>(), new LinkedList<>()
+        ));
+
+    }
+
+}
---
+++ AM19/AM19/src/test/java/it/polimi/ingsw/ModificationsTest.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw;
+import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ModificationsTest {
+    private Board board;
+
+    @BeforeEach
+    public void setUp(){
+        board = new Board("test",
+                new GameBoard(),
+                new ArrayList<>(),
+                new ArrayList<>()
+                );
+    }
+
+    @Test
+    public void discountTest(){
+        Discount effect = new Discount(new ResQuantity(new Coin(), 2));
+        effect.applyEffect(board);
+
+        assertEquals(board.getModifications().getDiscount(new Coin()), 2);
+    }
+    @Test
+    public void whiteMarbleTest(){
+        WhiteMarble effect = new WhiteMarble(new Coin());
+        effect.applyEffect(board);
+
+        assertTrue(board.getModifications().marbleTo(new Coin()));
+    }
+    @Test
+    public void extraSlotTest(){
+        ExtraSlot effect = new ExtraSlot(new ResQuantity(new Coin(), 2));
+        effect.applyEffect(board);
+
+    try{
+            assertEquals(board.getWarehouse().getCapacity(4), 2);
+            assertEquals(board.getWarehouse().getResource(4),new Coin());
+    }
+    catch(IllegalShelfException e){
+        assertTrue(false);
+        }
+    }
+
+}
