eee18dd1bf10200ad05f3ceb9924aa10bcabde14
andrea-chizzola <andrea.chizzola@mail.polimi.it>
2021-04-11 15:20:45 +0200

    Fixed Modifications Test

--- AM19/AM19/src/test/java/it/polimi/ingsw/ModificationsTest.java
+++ AM19/AM19/src/test/java/it/polimi/ingsw/ModificationsTest.java
@@ -10,11 +10,21 @@ public class ModificationsTest {
 
     @BeforeEach
     public void setUp(){
-        board = new Board("test",
-                new GameBoard(new ArrayList<String>(), new ArrayList<Integer>(), new ArrayList<ArrayList<VaticanReportSection>>()),
-                new ArrayList<>(),
-                new ArrayList<>()
-                );
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("firstPlayer");
+
+        ArrayList<VaticanReportSection> section1 = new ArrayList<>();
+        section1.add(0, new VaticanReportSection(5, 8, 2));
+        section1.add(1, new VaticanReportSection(12, 16, 3));
+        section1.add(2, new VaticanReportSection(19, 24, 4));
+
+        ArrayList<ArrayList<VaticanReportSection>> allSections = new ArrayList<>();
+        allSections.add(section1);
+
+        GameBoard gameBoard = new GameBoard(names, new ArrayList<>(), allSections);
+
+        board = gameBoard.getPlayers().get(0);
     }
 
     @Test
