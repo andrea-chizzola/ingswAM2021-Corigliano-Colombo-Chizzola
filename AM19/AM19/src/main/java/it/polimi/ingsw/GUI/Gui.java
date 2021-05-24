d1469ce657937585afd5a869e552b5e3e67890cd
andrea-chizzola <andrea.chizzola@mail.polimi.it>
2021-05-24 17:10:29 +0200

    Added Launcher class to start either server or client; Updated pom; Added Gui related classes

---
+++ AM19/AM19/src/main/java/it/polimi/ingsw/GUI/Gui.java
@@ -0,0 +1,40 @@
+package it.polimi.ingsw.GUI;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Gui extends Application  {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        scene = new Scene(loadFXML("/FXML/login.fxml"), 575, 534);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @Override
+    public void stop(){
+        Platform.exit();
+        System.exit(0);
+    }
+
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Gui.class.getResource(fxml));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
