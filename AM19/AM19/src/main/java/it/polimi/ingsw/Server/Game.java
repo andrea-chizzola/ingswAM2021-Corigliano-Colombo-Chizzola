de3fb1f9f639656909a73375b859f5bb392086e0
andrea-chizzola <andrea.chizzola@mail.polimi.it>
2021-05-24 17:16:08 +0200

    Managed reconnection for single player games

--- AM19/AM19/src/main/java/it/polimi/ingsw/Server/Game.java
+++ AM19/AM19/src/main/java/it/polimi/ingsw/Server/Game.java
@@ -116,9 +116,9 @@ public class Game {
      * @param connection represents the player's socket connection
      */
     public void addPlayer(String nickname, String socketId, ClientConnection connection){
-
+/*
         if(start)
-            messageHandler.reconnection(nickname);
+            messageHandler.reconnection(nickname);*/
         players.put(socketId, nickname);
         connections.put(nickname, connection);
         if(playersNumber == players.size()) start = true;
@@ -130,10 +130,10 @@ public class Game {
      * @param socketId represents the removed player's socket id
      */
     public void removePlayer(String socketId){
-
+/*
         if(start){
             messageHandler.disconnection(players.get(socketId));
-        }
+        }*/
         String nickname = players.get(socketId);
 
         connections.remove(nickname);
@@ -210,12 +210,35 @@ public class Game {
     }
 
     /**
+     * checks if after the timer at least one player has been reconnected to the game
+     */
+    public void startSuspensionTimer(){
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                if(getActualPlayers() == 0){
+                    System.out.println("[SERVER] No player reconnected. The game will now be closed.");
+                    gamesHandler.removeGame(getId(), new ArrayList<>(players.keySet()));
+                    System.out.println("[SERVER] Suspended game removed.");
+                }else{
+                    System.out.println("[SERVER] A player was reconnected to his game. The game will not be closed.");
+                }
+                timer.cancel();
+            }
+        };
+        timer.schedule(task, 30000);
+
+    }
+
+    /**
      * sets up the game creating all the necessary components
      */
     public void setUpGame(){
 
         System.out.println("[SERVER] Creating a new game...");
-
+/*
         ArrayList<String> names = new ArrayList<>(players.values());
         try {
             sendAll(MessageFactory.buildStartGame("Game is starting", names));
@@ -233,7 +256,7 @@ public class Game {
 
         this.messageHandler = new MessageHandler(gameBoard, virtualView);
 
-
+*/
         System.out.println("[SERVER] Game is ready to start");
 
     }
