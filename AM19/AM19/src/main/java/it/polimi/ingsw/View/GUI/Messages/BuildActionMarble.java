67f0966d1f8d4760877c6814d4681297a3a8fbc6
marco66colombo <80001772+marco66colombo@users.noreply.github.com>
2021-06-12 17:11:01 +0200

    New Javadoc in BuildMessage

--- AM19/AM19/src/main/java/it/polimi/ingsw/View/GUI/Messages/BuildActionMarble.java
+++ AM19/AM19/src/main/java/it/polimi/ingsw/View/GUI/Messages/BuildActionMarble.java
@@ -2,6 +2,11 @@ package it.polimi.ingsw.View.GUI.Messages;
 
 public class BuildActionMarble implements BuildMessage{
 
+    /**
+     * This methods builds an ActionMarble message
+     * @param accumulator it contains all the information useful to build the message
+     * @return the message built
+     */
     @Override
     public String buildMessage(Accumulator accumulator) {
         return accumulator.buildActionMarble();
--- AM19/AM19/src/main/java/it/polimi/ingsw/View/GUI/Messages/BuildLeaderUpdate.java
+++ AM19/AM19/src/main/java/it/polimi/ingsw/View/GUI/Messages/BuildLeaderUpdate.java
@@ -2,6 +2,12 @@ package it.polimi.ingsw.View.GUI.Messages;
 
 public class BuildLeaderUpdate implements BuildMessage{
 
+
+    /**
+     * This methods builds a LeaderUpdate message
+     * @param accumulator it contains all the information useful to build the message
+     * @return the message built
+     */
     @Override
     public String buildMessage(Accumulator accumulator) {
         return accumulator.buildLeaderUpdate();
